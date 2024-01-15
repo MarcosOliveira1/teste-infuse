f636f58a9540d52056f109db94d866e80d0575cb
MarcosOliveira1 <omarcosoliveira1997@gmail.com>
2024-01-15 18:05:09 -0300

    initiall commit

---
+++ src/main/java/com/teste/dev/infuse/teste/dev/java/WEB/infuse/Application.java
@@ -0,0 +1,13 @@
+package com.teste.dev.infuse.teste.dev.java.WEB.infuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/dev/infuse/teste/dev/java/WEB/infuse/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.dev.infuse.teste.dev.java.WEB.infuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
